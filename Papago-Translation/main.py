6b63e62b84d48bf0a7a52240c743557139ea4eea
dead-1ine <yang991211@naver.com>
2024-04-29 21:17:49 +0900

    feat: Add new workflow - Workflow Name: Papago Translation - What for: Papago Translation API usage to aligh with changes from NAVER to NAVER CLOUD Platform

---
+++ Papago-Translation/main.py
@@ -0,0 +1,77 @@
+import sys
+
+import ssl
+import json
+
+from urllib import request, parse
+
+
+# Naver Papago Translation API credentials
+client_id = 'YOUR_CLIENT_ID'
+client_secret = 'YOUR_CLIENT'
+
+def req_papago_translation_api(text, source='en', target='ko'):
+    data = f'source={source}&target={target}&text={parse.quote(text)}'
+
+    req = request.Request(
+        url='https://naveropenapi.apigw.ntruss.com/nmt/v1/translation',
+        headers={
+            'X-NCP-APIGW-API-KEY-ID': client_id,
+            'X-NCP-APIGW-API-KEY': client_secret
+        })
+    
+    response = request.urlopen(
+        req, 
+        data=data.encode('utf-8'), 
+        context=ssl._create_unverified_context()
+    )
+
+    res_code = response.getcode()
+
+    # TODO: handle error code with try-catch
+    if(res_code == 200):
+        response_body = json.loads(response.read().decode('utf-8'))
+    else:
+        print("Error Code:" + res_code)
+
+    return response_body['message']['result']['translatedText']
+
+
+def make_alfred_output(title, subtitle, arg=None):
+    return {
+        'title': title,
+        'subtitle': subtitle,
+        'icon': {'path': 'icon.png'},
+        'arg': arg,
+    }
+
+
+if __name__ == '__main__':
+    # call cli from Alfred Workflow - Script filter
+    input_text = sys.argv[1]
+
+    # Determine the source and target languages based on the input text
+    if input_text.isalpha():
+        source, target = 'en', 'ko' 
+    else:
+        source, target = 'ko', 'en'
+
+    # Translate the input text using the Papago Translation API
+    translate_text = req_papago_translation_api(
+        input_text,
+        source=source, 
+        target=target
+    )
+
+    alfred_output = make_alfred_output(
+        title=translate_text,
+        subtitle='Copy result to clipboard :)', 
+        arg=translate_text
+    )
+
+    alfred_json = json.dumps({
+        'items': [alfred_output]
+    })
+
+    sys.stdout.write(alfred_json)
+    sys.stdout.flush() # flush the stdout buffer
